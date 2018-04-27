708c01b5cc8265d1e2e570d9c084c4a1a063ca78
Lucia <lucymtzlemas@gmail.com>
2018-04-26 17:04:50 -0700

    borrar

--- main/error/error.asm
+++
@@ -1,9 +0,0 @@
-section .data
-    LabelError DB '*** Esa Opcion No Es Valida ***',0x0
-
-section .text
-printErrorLabel:
-    mov eax, LabelError ; cargamos el primer mensaje
-    call sprintLF       ; mandamos a llamar al "metodo" que imprime
-    call saltoDeLinea   ; 
-    jmp _start          ; regresando al principio
